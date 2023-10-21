9a1fac7f60e591d086f56eff4464eb7848fa78bb
Monalysa <monalysaklauck@gmail.com>
2023-10-21 19:04:14 -0300

    frist commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from datetime import time
+
+from models.create_invoices import CreateInvoices
+
+
+
+invoice = CreateInvoices()
+
+while True:
+    # Agende a execução a cada 3 horas
+    invoice.issue_invoices()
+    time.sleep(3 * 60 * 60)  # 3 horas em segundos
